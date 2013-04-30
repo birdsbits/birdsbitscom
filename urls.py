496b2293331d692dd33accca66e2d457d4995dd8
Sarah Bird <birdsarah@hawks.dreamhost.com>
2013-04-29 19:29:20 -0700

    First commit

---
+++ urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TemplateView.as_view(template_name="base/home.html"), name='home'),
+    url(r'^bbadit/', include(admin.site.urls)),
+)
+
